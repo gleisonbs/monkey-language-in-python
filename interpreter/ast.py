a5ea2af54df3e36e79b041b9183c22183655d201
Gleison Batista <gleisonbs@gmail.com>
2024-01-03 23:20:36 -0300

    refactor: add typing to ast

--- interpreter/ast.py
+++ interpreter/ast.py
@@ -1,20 +1,33 @@
 from interpreter.token import Token
+from typing import List, Optional
+
 
 class Program:
     def __init__(self):
-        self.statements = []
+        self.statements: List[Expression] = []
 
 class Expression:
     def __init__(self):
         ...
 
 class Identifier:
-    def __init__(self, token, value):
+    def __init__(self, token: Token, value: str):
         self.token = token
         self.value = value
 
+    def __str__(self) -> str:
+        return f"{self.token}, Value: {self.value}"
+
+    def token_literal(self) -> str:
+        return self.token.literal
+
 class LetStatement:
-    def __init__(self, token, identifier=None, expression=None):
+    def __init__(self, token: Token, 
+        identifier: Optional[Identifier] = None, 
+        expression: Optional[Expression] = None):
         self.token = token
         self.name = identifier
         self.value = expression
+
+    def __str__(self) -> str:
+        return f"{self.token}, {self.identifier}, {self.value}"
